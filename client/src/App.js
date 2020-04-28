ca4361ce79c92036d0900e161dbeb460c6ee1c50
Annalisa Macaluso <annalisamacaluso1@gmail.com>
2020-04-27 18:45:14 -0700

    added a login2 file

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/home";
 import Create from "./pages/createRecipe";
 import Library from "./pages/recipeLibrary";
 import NoPage from "./pages/noPage";
+import Login2 from "./components/Login2"
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route exact path={["/", "/home"]} component={Home} />
                 <Route exact path="/recipe/create" component={Create} />
                 <Route exact path="/recipe/library" component={Library} />
+                <Route exact path="/login2" component={Login2} />
                 <Route component={NoPage} />
             </Switch>
         </BrowserRouter>

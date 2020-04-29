f808a9327df33e2ae0e0e2b50bf79b1e913b6a6d
Annalisa Macaluso <annalisamacaluso1@gmail.com>
2020-04-28 17:36:51 -0700

    working on add recipe form and USDA API call

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Home from "./pages/home";
 import Create from "./pages/createRecipe";
 import Library from "./pages/recipeLibrary";
 import NoPage from "./pages/noPage";
+import Login2 from "./components/Login2"
+
+import "./App.css"
 
 function App() {
     return (
@@ -13,6 +16,7 @@ function App() {
                 <Route exact path={["/", "/home"]} component={Home} />
                 <Route exact path="/recipe/create" component={Create} />
                 <Route exact path="/recipe/library" component={Library} />
+                <Route exact path="/login2" component={Login2} />
                 <Route component={NoPage} />
             </Switch>
         </BrowserRouter>
--- client/src/components/about.js
+++ client/src/components/about.js
@@ -6,20 +6,20 @@ function About() {
         <div>
             <h1>ABOUT US</h1>
                 <br />
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">Text or an image about how our app is useful</p>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Title</h5>
+                            <p className="card-text">Text or an image about how our app is useful</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <p class="card-text">Text or an image about how our app is useful</p>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Title</h5>
+                            <p className="card-text">Text or an image about how our app is useful</p>
                         </div>
                     </div>
                 </div>

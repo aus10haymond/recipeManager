8eed37fe314eb7ca2890b4edb4cc588d33b8339e
Austen Haymond <ahaymon17@gmail.com>
2020-04-30 20:19:31 -0700

    fixed react error

--- client/src/pages/createRecipe.js
+++ client/src/pages/createRecipe.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-​
+import React, { useState, useEffect } from "react";​
+
 import NavBar from "../components/navBar";
 import Sidebar from "../components/sideNav";
 import SideBar from "../components/sideNav";

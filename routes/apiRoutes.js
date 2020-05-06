40a3976eca0c87ee4d43b8645df686f96f3909e3
ljv29 <jumpswervelogan@gmail.com>
2020-05-05 21:24:25 -0700

    merging

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,24 +4,27 @@ const db = require("../models");
 
 function apiRoutes(app) {
   // routes for user
-  // app.post("/signup", ({ body }, res) => {
-  //   db.User.create(body)
-  //     .then(results => {
-  //       res.json(results);
-  //     })
-  //     .catch(err => {
-  //       res.json(err);
-  //     });
-  // });
-  // app.post("/login", ({ body }, res) => {
-  //   db.Us.create(body)
-  //     .then(results => {
-  //       res.json(results);
-  //     })
-  //     .catch(err => {
-  //       res.json(err);
-  //     });
-  // });
+  app.post("/signup", ({ body }, res) => {
+    db.User.create(body)
+      .then(results => {
+        res.json(results);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.post("/login", ({ body }, res) => {
+    db.User.create(body)
+      .then(results => {
+        res.json(results);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  
   // routes for ingreidents
   app.get("/all", (req, res) => {
     db.Ingredient.find({})

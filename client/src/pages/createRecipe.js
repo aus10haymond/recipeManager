1a7ab64e9ea817a5234fb4629a736759395a452b
Annalisa Macaluso <annalisamacaluso1@gmail.com>
2020-05-05 17:41:16 -0700

    create recipe %, delete ing, fix enter issue

--- client/src/pages/createRecipe.js
+++ client/src/pages/createRecipe.js
@@ -5,7 +5,6 @@ import SideBar from "../components/sideNav";
 import API from "../utils/API";
 import ExportCSV from "../components/exportCSV";
 
-
 function Create() {
   //console.log("api key", process.env.FOOD_DATA_APIKEY)
 
@@ -22,6 +21,19 @@ function Create() {
     }
   }, [newInput]);
 
+  useEffect(() => {
+    let ingredientsCopy = [...currentIngredients];
+
+    let totalWeight = 0;
+    ingredientsCopy.forEach(ingredient=>{
+      totalWeight = totalWeight + ingredient.weightInRecipe
+    })
+
+    totalWeight = console.log("total weight = " + totalWeight)
+
+    //setCurrentIngredients(ingredientsCopy);
+  }, [currentIngredients]);
+
   const remakeIngredient = (ingredient) => {
     let newIngredient = {
       name: ingredient.description,
@@ -29,25 +41,40 @@ function Create() {
       nutritioninfo: ingredient.foodNutrients,
       totalWeight: "",
       totalCost: "",
-      weightInRecipe: ""
+      weightInRecipe: "",
+      costInRecipe: ""
     };
 
     setCurrentIngredients([...currentIngredients, newIngredient]);
   };
 
-  const addStuffToIngredient = event => {
+  const addStuffToIngredient = (event) => {
     let ingredientsCopy = [...currentIngredients];
     let value = event.target.value;
     let name = event.target.name;
     let index = event.target.id;
-    console.log(value, name, index)
+    console.log(value, name, index);
 
-    ingredientsCopy[index][name] = value
+    ingredientsCopy[index][name] = value;
     // ingredientsCopy[index]["totalWeight"]
     // ingredientsCopy[index].totalWeight
+    
+    ingredientsCopy[index].costInRecipe = (ingredientsCopy[index].totalCost /
+      ingredientsCopy[index].totalWeight) *
+      ingredientsCopy[index].weightInRecipe;
+
 
     setCurrentIngredients(ingredientsCopy);
-  }
+  };
+
+  const deleteIngredient = (event) => {
+    let ingredientsCopy = [...currentIngredients];
+    let index = event.target.id;
+
+    ingredientsCopy.splice(index, 1);
+
+    setCurrentIngredients(ingredientsCopy);
+  };
 
   //gonna be made in this card:
   //list of ingredients including % of recipe by weight up to 100% (user adds these by gram or w/e)
@@ -83,7 +110,10 @@ function Create() {
                 Create a new recipe and add it to your book.
               </h3>
             </div>
-            <ExportCSV csvData={currentIngredients} fileName={"exportedRecipe"} />
+            <ExportCSV
+              csvData={currentIngredients}
+              fileName={"exportedRecipe"}
+            />
             <table id="excelTable" className="table table-hover">
               <thead>
                 <tr>
@@ -94,17 +124,14 @@ function Create() {
                   <th scope="col">Weight in Recipe (grams)</th>
                   <th scope="col">Weight %</th>
                   <th scope="col">Cost in Recipe</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
                 {currentIngredients.map((ingredient, i) => (
                   <tr scope="row" key={i}>
-                    <td>
-                      {ingredient.name}
-                    </td>
-                    <td>
-                      {ingredient.brand}
-                    </td>
+                    <td>{ingredient.name}</td>
+                    <td>{ingredient.brand}</td>
                     <td>
                       <input
                         id={i}
@@ -131,28 +158,32 @@ function Create() {
                     </td>
                     <td></td>
                     <td>
-                    {currentIngredients[i].totalCost / currentIngredients[i].totalWeight * currentIngredients[i].weightInRecipe}
+                      {ingredient.costInRecipe}
+                    </td>
+                    <td>
+                      <span id={i} onClick={deleteIngredient} style={{ color: "red", cursor: "pointer" }}>
+                        X
+                      </span>
                     </td>
                   </tr>
                 ))}
-                <tr>
-
-                </tr>
+                <tr></tr>
               </tbody>
             </table>
-            <form>
-              <div className="form-group">
-                <label htmlFor="newIngredients">Search New Ingredients</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="newIngredients"
-                  placeholder="cheese"
-                  value={newInput}
-                  onChange={(event) => setNewInput(event.target.value)}
-                />
-              </div>
-              {/* <div className="form-group">
+            <button>Save</button>
+            {/* <form onSubmit={e=>e.preventDefault()}> */}
+            <div className="form-group">
+              <label htmlFor="newIngredients">Search New Ingredients</label>
+              <input
+                type="text"
+                className="form-control"
+                id="newIngredients"
+                placeholder="cheese"
+                value={newInput}
+                onChange={(event) => setNewInput(event.target.value)}
+              />
+            </div>
+            {/* <div className="form-group">
                       <label htmlFor="exampleFormControlSelect1">Example select</label>
                       <select className="form-control" id="exampleFormControlSelect1">
                         <option>1</option>
@@ -176,8 +207,8 @@ function Create() {
                       <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
                       <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div> */}
-              <button>Search</button>
-            </form>
+
+            {/* </form> */}
           </div>
           {ingredientRes.map((ingredient) => (
             <div>

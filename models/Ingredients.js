97d03dacf12a4a49ab49aad9c196c963dd66f5bb
ljv29 <jumpswervelogan@gmail.com>
2020-05-06 19:52:55 -0700

    took out array in models

--- models/Ingredients.js
+++ models/Ingredients.js
@@ -6,11 +6,9 @@ const IngredientSchema = new Schema({
   brand: { type: String },
   cost: { type: String },
   size_weight: { type: String },
-  // nutritioninfo: [{
   nutrientName: { type: String },
   unitName: { type: String },
   value: { type: String },
-  // }]
 });
 
 const Ingredient = mongoose.model("Ingredient", IngredientSchema);

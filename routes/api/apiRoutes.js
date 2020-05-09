ca21faeac4a22e656e98cfddb0a8b7ff4875b47a
ljv29 <jumpswervelogan@gmail.com>
2020-05-08 19:07:32 -0700

    added passport route for logging out, as well as a post route for recipe. also post route for login is using passport.authenticate function

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,133 +1,168 @@
 const db = require("../../models");
 const router = require("express").Router();
+const passport = require("../../config/passport-config");
 
 
-  // routes for user
-  router.post("/signup", ({ body }, res) => {
-    const user = new db.User(body);
-    user.generateHash(body.password);
+// PASSPORT/user routes 
+router.post("/signup", ({ body }, res) => {
+  const user = new db.User(body);
+  user.generateHash(body.password);
+  user.validPassword(body.password);
 
-    db.User.create(user)
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  router.post("/login", ({ body }, res) => {
-    db.User.create(body)
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-
-  // routes for ingreidents
-  router.get("/all", (req, res) => {
-    db.Ingredient.find({})
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  router.get("/recipe/all", (req, res) => {
-    db.Recipe.find({})
-      .populate("ingredients")
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  router.post("/submit", ({ body }, res) => {
-    db.Ingredient.create(body)
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  //full route: /api/submit/many
-  router.post("/submit/many", ({ body }, res) => {
-    db.Ingredient.insertMany(body.ingredients)
-      .then(ingredientResults => {
-
-        console.log(ingredientResults)
-        let idArr = ingredientResults.map(ingredient=>ingredient._id)
-        console.log(idArr);
-
-        return db.Recipe.create({
-          name: body.name,
-          directions: body.directions,
-          ingredients: idArr
-        })
-      }
-      )
-      .then(moreResults=>{
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  router.get("/find/:id", (req, res) => {
-    db.Ingredient.findOne({
-      _id: req.params.id
+  db.User.create(user)
+    .then(results => {
+      res.json(results);
+      res.redirect('/login')
     })
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+    .catch(err => {
+      res.json(err);
+      res.redirect('/signup')
+    });
+});
 
-  router.post("/update/:id", ({ body, params }, res) => {
-    db.Ingredient.findByIdAndUpdate(params.id, {
-      $set: body
+router.post("/login", passport.authenticate("local"), ({ body }, res) => {
+  db.User.create(body)
+    .then(results => {
+      res.json(results);
     })
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.delete("/delete/:id", (req, res) => {
-    db.Ingredient.remove({
-      _id: req.params.id
+// link this to log out button in front end.
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get("/api/user_data", function (req, res) {
+  if (!req.user) {
+    // the user is not logged in, send back an empty object
+    res.json({});
+  } else {
+    // otherwise send back the user's email and id
+    // sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      _id: req.user.id
+    });
+  }
+});
+
+
+// recipes routes
+router.get("/recipe/all", (req, res) => {
+  db.Recipe.find({})
+    .populate("ingredients")
+    .then(results => {
+      res.json(results);
     })
-      .then(results => {
-        res.json(results);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  router.delete("/clearall", (req, res) => {
-    db.Ingredient.remove({})
-      .then(results => {
-        res.json(results);
+router.post("/recipe/post", ({ body }, res) => {
+  db.Recipe.create(body)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+// ingredient routes
+router.get("/all", (req, res) => {
+  db.Ingredient.find({})
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/submit", ({ body }, res) => {
+  db.Ingredient.create(body)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/submit/many", ({ body }, res) => {
+  db.Ingredient.insertMany(body.ingredients)
+    .then(ingredientResults => {
+
+      console.log(ingredientResults)
+      let idArr = ingredientResults.map(ingredient => ingredient._id)
+      console.log(idArr);
+
+      return db.Recipe.create({
+        name: body.name,
+        directions: body.directions,
+        ingredients: idArr
       })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+    }
+    )
+    .then(moreResults => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/find/:id", (req, res) => {
+  db.Ingredient.findOne({
+    _id: req.params.id
+  })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/update/:id", ({ body, params }, res) => {
+  db.Ingredient.findByIdAndUpdate(params.id, {
+    $set: body
+  })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  db.Ingredient.remove({
+    _id: req.params.id
+  })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/clearall", (req, res) => {
+  db.Ingredient.remove({})
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 
 module.exports = router;
